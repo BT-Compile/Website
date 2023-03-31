def61c8322070e08ce78834a00ce7975845b9640
Ben Trang <121653356+BT-Compile@users.noreply.github.com>
2023-03-31 11:02:28 -0400

    3/31/2023 11:01am
    worked on getting onmouseover to work for nav

---
+++ Site/root.js
@@ -0,0 +1,8 @@
+function hover() {
+    document.getElementById('about').style.background = "white"
+    document.getElementById('about').style.color = "black"
+}
+
+function offhover() {
+    document.
+}
