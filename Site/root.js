fe164c0ebfdeb8fc5a5ba609fe2adfeab1647cff
Ben Trang <121653356+BT-Compile@users.noreply.github.com>
2023-04-05 13:55:43 -0400

    worked on root page
    added 3rd container

--- Site/root.js
+++ Site/root.js
@@ -8,6 +8,16 @@ function offhover(id) {
   document.getElementById(id).style.color = "";
 }
 
+function footer() {
+  var update = new Date(document.lastModified)
+  var modMonth = update.getMonth() + 1
+  var modDate = update.getDate()
+  var modYear = update.getFullYear()
+  document.write("Benjamin Trang &#169; " + modMonth + "/" + modDate + "/" + modYear);
+}
+
+
+/* below is all js for the image slider*/
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -23,8 +33,12 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("sliderimg");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
